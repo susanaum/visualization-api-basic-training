11172da06372520174cdf714bc1b399023500c32
Urdiales Monje, Susana <smonje@microstrategy.com>
2022-07-21 19:12:15 +0100

    Draw bar charts

--- SampleBarChart/src/SampleBarChart.js
+++ SampleBarChart/src/SampleBarChart.js
@@ -7,7 +7,6 @@ mstrmojo.requiresDescsWPrefix("SampleBarChart.", 10, 11, 12, 13);
 // mojo module
 mstrmojo.requiresCls("mstrmojo.CustomVisBase");
 
-const { GraphicModel } = mstrmojo.customviz;
 const { ENUM_RAW_DATA_FORMAT } = mstrmojo.models.template.DataInterface;
 
 mstrmojo.plugins.SampleBarChart.SampleBarChart = mstrmojo.declare(
@@ -16,10 +15,8 @@ mstrmojo.plugins.SampleBarChart.SampleBarChart = mstrmojo.declare(
   {
     scriptClass: "mstrmojo.plugins.SampleBarChart.SampleBarChart",
     cssClass: "samplebarchart",
-    errorMessage:
-      "Either there is not enough data to display the visualization or the visualization configuration is incomplete.",
-    errorDetails:
-      "This visualization requires one or more attributes and one metric.",
+    errorMessage:"Either there is not enough data to display the visualization or the visualization configuration is incomplete.",
+    errorDetails:"This visualization requires one or more attributes and one metric.",
     useRichTooltip: false,
     reuseDOMNode: false,
     draggable: false,
@@ -30,31 +27,7 @@ mstrmojo.plugins.SampleBarChart.SampleBarChart = mstrmojo.declare(
       const defaultPropertyValues = getDefaultProperties();
       this.setDefaultPropertyValues(defaultPropertyValues);
     },
-    createGraphicModels() {
-      const rawData = this.dataInterface.getRawData(
-        ENUM_RAW_DATA_FORMAT.ROWS_ADV,
-        {
-          hasSelection: true,
-          hasTitleName: true,
-          hasThreshold: true,
-          additionalAttrIds: this.additionalAttrIds,
-        }
-      );
 
-      return rawData.map((row) => {
-        const { values, headers } = row;
-        const graphicModel = new GraphicModel();
-
-        graphicModel.idValueMapping = row.idValueMapping;
-        graphicModel.setCustomProperties({
-          text: headers[0].name,
-          value: values[0].rv,
-        });
-        graphicModel.setId(this.getSelectorHash(row.metricSelector));
-        graphicModel.setSelector(row.metricSelector, true);
-        return graphicModel;
-      });
-    },
     clearDomNode() {
       const host = this;
       const { domNode } = host;
@@ -63,14 +36,6 @@ mstrmojo.plugins.SampleBarChart.SampleBarChart = mstrmojo.declare(
     plot(appliedProperties = null) {
       const host = this;
 
-      host.clearDomNode();
-
-      const graphicModels = host.getGraphicModels();
-
-      if (graphicModels.length === 0) {
-        return;
-      }
-
       let properties;
       if (appliedProperties) {
         properties = adjustProperties(appliedProperties, host);
@@ -78,81 +43,204 @@ mstrmojo.plugins.SampleBarChart.SampleBarChart = mstrmojo.declare(
         const savedProperties = JSON.parse(host.getProperty("unifiedProperty"));
         properties = adjustProperties(savedProperties, host);
       }
-
-      // render part
-      const {
-        showAttribute,
-        showMetric,
-        attributeBackgroundColor,
-        metricBackgroundColor,
-        attributeFontFamily,
-        attributeFontStyle,
-        attributeFontColor,
-        attributeFontSize,
-        metricFontFamily,
-        metricFontStyle,
-        metricFontColor,
-        metricFontSize,
-      } = properties;
-
-      const addStyle = (
-        element,
-        fontFamily,
-        fontColor,
-        fontSize,
-        fontStyle
-      ) => {
-        element.style.fontFamily = fontFamily;
-        element.style.color = fontColor;
-        element.style.fontSize = `${fontSize}px`;
-        const [isBold, isItalic, isUnderlined, isCrossedOut] = fontStyle;
-        element.style.fontWeight = isBold ? "bold" : "normal";
-        element.style.fontStyle = isItalic ? "italic" : "normal";
-        const textDecorationOptions = [];
-        if (isUnderlined) textDecorationOptions.push("underline");
-        if (isCrossedOut) textDecorationOptions.push("line-through");
-        const textDecoration = textDecorationOptions.join(" ");
-        element.style.textDecoration = textDecoration;
-      };
-
-      graphicModels.forEach((graphicModel) => {
-        const container = document.createElement("div");
-        container.classList.add("container");
-        const attributeContainer = document.createElement("div");
-        attributeContainer.classList.add("attributeContainer");
-        attributeContainer.style.backgroundColor = attributeBackgroundColor;
-        const attributeContainerText = document.createTextNode(
-          showAttribute ? graphicModel.text : ""
-        );
-        addStyle(
-          attributeContainer,
-          attributeFontFamily,
-          attributeFontColor,
-          attributeFontSize,
-          attributeFontStyle
-        );
-        attributeContainer.appendChild(attributeContainerText);
-
-        const metricContainer = document.createElement("div");
-        metricContainer.classList.add("metricContainer");
-        metricContainer.style.backgroundColor = metricBackgroundColor;
-        const metricContainerText = document.createTextNode(
-          showMetric ? graphicModel.value : ""
-        );
-        addStyle(
-          metricContainer,
-          metricFontFamily,
-          metricFontColor,
-          metricFontSize,
-          metricFontStyle
-        );
-        metricContainer.appendChild(metricContainerText);
-
-        container.appendChild(attributeContainer);
-        container.appendChild(metricContainer);
-
-        host.domNode.appendChild(container);
-      });
+      let data = getData();
+      displayBarChart(data);
     },
-  }
-);
+    /**
+     * Extracts the data used in the visualization and constructs the data structure
+     * @returns 
+     */
+    getData(){
+      let rawData = this.dataInterface.geRawData(ENUM_RAW_DATA_FORMAT.ROWS_ADV, {hasThreshold:true});
+      for(let att =0; att<rawData.length; att++){
+				let row = widgetData[att];
+					data.push({
+						attributeSelector: row.attributeSelector,
+						attribute : row.name,
+						metric_value : (row.values[0].rv !== "") ? Number(row.values[0].rv) : 0,
+						color: (rowThresholds.values[0].threshold) ? rowThresholds.values[0].threshold.fillColor : colorArray[0],  
+						tooltipValue: (row.values[0].v !== "") ? Number(row.values[0].v) : 0
+					});
+			}	
+			return data;
+    }, 
+    /**
+     * Draws the bar chart in the visualization container.
+     * @param {*} data 
+     */
+    displayBarChart(data){
+
+      const visualizationContainer = this;
+      const position = setPositionParameters(me, data);
+
+      var chart = d3.select(visualizationContainer.domNode).append("svg")
+      .attr("class", "BasicVisualization")
+      .attr("width", visualizationContainer.width)
+      .attr("height", visualizationContainer.height)
+      .append("g").attr("transform", "translate(" + position.margin.left + "," + position.margin.top + ")");
+
+      drawBars(chart, data, position, visualizationContainer);
+			drawAxis(chart, data, position, visualizationContainer);
+
+    },
+    /**
+     * Draw bars of the bar chart
+     * @param {*} chart 
+     * @param {*} data 
+     * @param {*} parameters 
+     * @param {*} me 
+     */
+    drawBars(chart, data, parameters, me){
+        var x = parameters.x, y = parameters.y;
+        var barWidth = x.bandwidth()/4;
+        
+        chart.selectAll(".rect")
+          .data(data)
+          .enter()
+          .append("rect")
+          // bar width
+          .attr("width", function (d){
+            return barWidth;
+          })
+          .attr("height", function(d) { 
+            return Math.abs(y(0) - y(d.metric_value));
+          })
+          // bar starting point on x axis
+          .attr("x", function(d) {
+            return x(d.attribute) + pos + (x.bandwidth() - barWidth)/2;
+          })
+          // bar starting point of the y axis
+          .attr("y", function(d) { 
+            return Math.min(y(0), y(d.metric_value));
+          })
+          // bar color
+          .style("fill", function(d) { 
+            return d.color;
+          });			
+    },
+    /**
+     * Draw 
+     * @param {*} chart 
+     * @param {*} data 
+     * @param {*} position 
+     * @param {*} me 
+     */
+    drawAxis(chart, data, position, me){
+
+      var x = position.x, y = position.y;
+      
+      var xAxis = d3.axisBottom(x)
+        .tickSize(0)
+        .tickFormat(function(d){return d;});
+      var yAxis = d3.axisLeft(y)
+        .tickSize(0)
+        .tickFormat(function(i){return i;});
+
+
+      // draw x axis
+      chart.append("g")
+        .attr("class", "xAxis")
+        .attr("stroke", "#000")
+        // set the x axis at the zero vertical value
+        .attr("transform", function() {
+          return "translate(0," + y(0) + ")";
+        })
+        .call(xAxis)
+        .selectAll("text")  
+        .style("text-anchor", "middle")
+        .attr("font-family","Segoe")
+        .attr("font-size", "11px")
+        .attr("dy", function (d) {
+          return ".8em";
+        })
+        .attr("dx", function (d) {
+          return "-0.5em";
+        })
+        .attr("fill", function(d) {
+          return "#000";
+        })
+        .attr("stroke", "none");
+      
+    
+      
+      
+    // draw x axis
+      chart.append("g")
+        .attr("class", "yAxis")
+        .attr("stroke", "#000")
+        // set the x axis at the zero vertical value
+        .attr("transform", function() {
+          return "translate( "+ x(0) + ",0)";
+        })
+        .call(yAxis)
+        .selectAll("text")  
+        .style("text-anchor", "middle")
+        .attr("font-family","Segoe")
+        .attr("font-size", "10px")
+        .attr("dx", function (i) {
+          return "-2.5em";
+        })
+        .attr("dy", function (i) {
+          return "0.2em";
+        })
+        .attr("fill", function(i) {
+          return "#000";
+        })
+        .attr("stroke", "none");
+    
+    
+      drawTicks(data, y, chart, x);
+    
+    },
+    /**
+     * Draw tickes in the bar chart axis
+     * @param {*} data 
+     * @param {*} y 
+     * @param {*} chart 
+     * @param {*} x 
+     */
+    drawTicks(data, y, chart, x) {
+
+      var ticks = [0];
+      var highestValue = findMaximum(data);
+      for (var index = 0; index < highestValue; index += 500000) {
+        ticks.push(y(index));
+      }
+    
+      chart.selectAll(".customTicks")
+        .data(ticks)
+        .enter()
+        .append("line")
+        .attr("stroke", "#000")
+        .attr("y1", function (d) { return d; })
+        .attr("y2", function (d) { return d; })
+        .attr("x1", function () { return x(0); })
+        .attr("x2", function () { return -5; });
+    },
+    findMaximum(data) {
+      var highestValue = 0;
+      data.forEach(function(item){
+        if(item.metric_value>highestValue){
+          highestValue = item.metric_value;
+        }
+      });
+      return highestValue;
+    },
+    setPositionParameters(me, data){
+      let highestValue = findMaximum(data);
+       
+      let x = d3.scaleBand().rangeRound([0,width]).padding(0.05, 0.5)
+              .domain(data.map(function(d){
+                return d.attribute;
+              }));
+      
+      let y = d3.scaleLinear().range([height, 0])
+              .domain([0, highestValue[1]]);
+
+      return {
+        x : x,
+        y : y
+      };
+    },
+}());
+
