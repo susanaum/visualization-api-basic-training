9b6cc6ae66981a5c25fa30689db7d6200f273147
Urdiales Monje, Susana <smonje@microstrategy.com>
2022-07-21 19:16:12 +0200

    Modify config file

--- SampleBarChart/config.js
+++ SampleBarChart/config.js
@@ -6,7 +6,7 @@ module.exports = {
   // ####################################################
 
   // the dir path of MicroStrategy plugins, relative or absolute path are both valid
-  path_of_plugins: 'plugins',
+  path_of_plugins: '/Applications/MicroStrategy Workstation.app/Contents/Frameworks/OneTierDashboardWindow.framework/Versions/A/Resources/code/plugins',
   // Whether you wanna manage WEB-INF altogether by yourself.
   // By default, files(e.g. messageBundle, styleCatalog.xml and visualizations.xml) in WEB-INF
   // would be created and updated based on this config file.
@@ -18,17 +18,17 @@ module.exports = {
   // As default, this value is same as plugin_name.
   // But if plugin_name is D3WordCloud, then 'Word Cloud' is more readable and suitable as visualization_name.
   visualization_name: 'SampleBarChart',
-  available_for_reports: true,
-  available_for_documents: true,
+  available_for_reports: false,
+  available_for_documents: false,
   available_for_dossiers: true,
   categories: 'Custom', // category of the custome viz in gallery.
   desc_id: '', // id of viz description string defined in i18n message bundle.
   summary_id: '', // id of viz summary string defined in i18n message bundle.
   use_case_id: '', // id of viz use case string defined in i18n message bundle.
   requirements_id: '', // id of viz requirement string defined in i18n message bundle.
-  summary: '', // default value of viz summary string defined in i18n message bundle.
-  use_case: '', // default value of viz use case string defined in i18n message bundle.
-  requirements: '', // default value of viz requirements string defined in i18n message bundle.
+  summary: 'Simple Bar Chart', // default value of viz summary string defined in i18n message bundle.
+  use_case: 'Represent KPI value per attribute element', // default value of viz use case string defined in i18n message bundle.
+  requirements: 'Uses 1 ##attributes and 1 ##metric', // default value of viz requirements string defined in i18n message bundle.
   format_panel_build_entry: './formatPanelConfig/config.js', // the build entry file of new format panel for the viz.
   use_react_config: true //Use react format panel.
 };
